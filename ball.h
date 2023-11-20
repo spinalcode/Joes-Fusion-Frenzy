ccbfc8762e2c213908ed0e12960a2ee5ac4b9e12
Neil <spinal_cord@yahoo.com>
2023-11-20 22:30:40 +0000

    Add files via upload

--- ball.h
+++ ball.h
@@ -1,8 +1,8 @@
-#define NUM_BALLS 1
-#define DAMPING (0.7 * 256)
-#define GRAVITY (0.6 * 256)
+#define NUM_BALLS 5
+#define DAMPING 0.7
+#define GRAVITY 0.6
 #define MOUSE_SIZE 5
-#define SPEED (0.6 * 256)
+#define SPEED 1
 #define TWO_PI (3.14159 * 2)
 
 typedef struct {
@@ -13,11 +13,9 @@ typedef struct {
 } Ball;
 
 Ball balls[NUM_BALLS];
-//int mouseDown = 0;
-int mouseX = 0;
-int mouseY = 0;
-int canvasWidth = 220 << 8;
-int canvasHeight = 176 << 8;
+
+int canvasWidth = 220;
+int canvasHeight = 176;
 
 void applyForce(Ball *ball, int delta) {
     delta *= delta;
@@ -36,8 +34,8 @@ void applyForce(Ball *ball, int delta) {
 }
 
 void verlet(Ball *ball) {
-    int nx = (ball->x * 2) - ball->px;
-    int ny = (ball->y * 2) - ball->py;
+    float nx = (ball->x * 2) - ball->px;
+    float ny = (ball->y * 2) - ball->py;
 
     ball->px = ball->x;
     ball->py = ball->y;
@@ -48,12 +46,13 @@ void verlet(Ball *ball) {
 
 
 void drawBall(Ball *ball) {
-    drawSprite(ball->x >> 8, ball->y >> 8, joe_03, 128, 8);
+    drawSprite(ball->x - ball->radius, ball->y - ball->radius, joe_03, 128, 8);
 }
 
 
 void resolveCollisions(int ip) {
-    int i, n, diff_x, diff_y, dist, real_dist, depth_x, depth_y;
+    int i, n;
+    float diff_x, diff_y, dist, real_dist, depth_x, depth_y;
     Ball *ball_1, *ball_2;
     i = NUM_BALLS;
 
@@ -71,15 +70,15 @@ void resolveCollisions(int ip) {
             diff_x = ball_1->x - ball_2->x;
             diff_y = ball_1->y - ball_2->y;
 
-            int length = diff_x * diff_x + diff_y * diff_y;
+            float length = diff_x * diff_x + diff_y * diff_y;
             dist = sqrt(length);
             real_dist = dist - (ball_1->radius + ball_2->radius);
 
             if (real_dist < 0) {
-                int vel_x1 = ball_1->x - ball_1->px;
-                int vel_y1 = ball_1->y - ball_1->py;
-                int vel_x2 = ball_2->x - ball_2->px;
-                int vel_y2 = ball_2->y - ball_2->py;
+                float vel_x1 = ball_1->x - ball_1->px;
+                float vel_y1 = ball_1->y - ball_1->py;
+                float vel_x2 = ball_2->x - ball_2->px;
+                float vel_y2 = ball_2->y - ball_2->py;
 
                 depth_x = diff_x * (real_dist / dist);
                 depth_y = diff_y * (real_dist / dist);
@@ -91,8 +90,8 @@ void resolveCollisions(int ip) {
                 ball_2->y += depth_y * 0.5;
 
                 if (ip) {
-                    int pr1 = DAMPING * (diff_x * vel_x1 + diff_y * vel_y1) / length;
-                    int pr2 = DAMPING * (diff_x * vel_x2 + diff_y * vel_y2) / length;
+                    float pr1 = DAMPING * (diff_x * vel_x1 + diff_y * vel_y1) / length;
+                    float pr2 = DAMPING * (diff_x * vel_x2 + diff_y * vel_y2) / length;
 
                     vel_x1 += pr2 * diff_x - pr1 * diff_x;
                     vel_x2 += pr1 * diff_x - pr2 * diff_x;
@@ -117,11 +116,11 @@ void check_walls() {
         Ball *ball = &balls[i];
 
         if (ball->x < ball->radius) {
-            int vel_x = ball->px - ball->x;
+            float vel_x = ball->px - ball->x;
             ball->x = ball->radius;
             ball->px = ball->x - vel_x * DAMPING;
         } else if (ball->x + ball->radius > canvasWidth) {
-            int vel_x = ball->px - ball->x;
+            float vel_x = ball->px - ball->x;
             ball->x = canvasWidth - ball->radius;
             ball->px = ball->x - vel_x * DAMPING;
         }
@@ -158,8 +157,8 @@ void updateBalls() {
 void initBalls(){
     int i;
     for (i = 0; i < NUM_BALLS; i++) {
-        balls[i].x = (rand() % (canvasWidth>>8));
-        balls[i].y = (rand() % (canvasHeight>>8));
+        balls[i].x = (rand() % canvasWidth);
+        balls[i].y = (rand() % canvasHeight);
         balls[i].px = balls[i].x;
         balls[i].py = balls[i].y;
         balls[i].radius = 13;//rand() % 20 + 10;
